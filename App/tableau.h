f556ca51cc10b71055a0d758203c2ed74c30b959
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2022-10-10 13:04:06 -0400

    simplification unique nlogn avec quicksort

--- App/tableau.h
+++ App/tableau.h
@@ -1,4 +1,4 @@
-/** 
+/**
  * Squelette pour classe générique Tableau<T>.
  * TP1 -- Tableau dynamique générique
  * UQAM / Département d'informatique
@@ -11,115 +11,123 @@
 #include <assert.h>
 
 template <class T>
-class Tableau{
+class Tableau
+{
 
-  public:
-    Tableau(int capacite_initiale=4);
-		Tableau(const Tableau & autre);
+public:
+    Tableau(int capacite_initiale = 4);
+    Tableau(const Tableau &autre);
     ~Tableau();
 
-
-	// redimensionner un tablea en doublant sa capacite
+    // redimensionner un tablea en doublant sa capacite
     void redimensionner(int nouvCapacite);
 
-	  /** 
-		 * description:  ajoute un élément à la fin.
-		 *
-		 * @param  			 une référence vers un paramètre de type T.
-		 */
-    void ajouter(const T & element);
-  
-		/** 
-		 * description:  retourne le nombre d'éléments dans le 
-		 * 							 tableau. Cette fonction ne doit pas modifier 
-		 * 							 le tableau courant.
-		 *
-		 * @return       un entier représentant la taille du tableau 
-		 *               courant.
-		 */
+    /**
+     * description:  ajoute un élément à la fin.
+     *
+     * @param  			 une référence vers un paramètre de type T.
+     */
+    void ajouter(const T &element);
+
+    /**
+     * description:  retourne le nombre d'éléments dans le
+     * 							 tableau. Cette fonction ne doit pas modifier
+     * 							 le tableau courant.
+     *
+     * @return       un entier représentant la taille du tableau
+     *               courant.
+     */
     int taille() const;
 
-		/** 
-		 * description:  insère un élément à la position 'index'. Les
-		 * 							 éléments à partir de index sont décalés d'une 
-		 * 							 postion vers la droite.
-		 *
-		 * @param        - une référence vers un paramètre de type T.
-		 * 							 - un entier représentant la position où sera 
-		 * 							 inséré l'élément.
-		 */
-    void inserer(const T & element, int index = 0);
+    /**
+     * description:  insère un élément à la position 'index'. Les
+     * 							 éléments à partir de index sont décalés d'une
+     * 							 postion vers la droite.
+     *
+     * @param        - une référence vers un paramètre de type T.
+     * 							 - un entier représentant la position où sera
+     * 							 inséré l'élément.
+     */
+    void inserer(const T &element, int index = 0);
 
-		/** 
-		 * description:  enlève un élément à la position 'index'. Les
-		 * 							 éléments après index sont décalés d'une postion
-		 * 							 vers la gauche.
-		 *
-		 * @param        un entier représentant la position d'où sera 
-		 * 						 	 enlevé l'élément.
-		 */
+    /**
+     * description:  enlève un élément à la position 'index'. Les
+     * 							 éléments après index sont décalés d'une postion
+     * 							 vers la gauche.
+     *
+     * @param        un entier représentant la position d'où sera
+     * 						 	 enlevé l'élément.
+     */
     void enlever(int index = 0);
 
-		/** 
-		 * description:  compte le nombre d'occurrence d'un élément dans
-		 * 							 le tableau. Cette fonction ne doit pas modifier 
-		 * 							 le tableau courant. 
-		 *
-		 * @param        une référence vers un paramètre de type T.
-		 *
-		 * @return       un entier représentant le nombre de fois que
-		 * 							 l'élément apparaît dans le tableau courant.
-		 */
-    int occurrence(const T & element) const;
+    /**
+     * description:  compte le nombre d'occurrence d'un élément dans
+     * 							 le tableau. Cette fonction ne doit pas modifier
+     * 							 le tableau courant.
+     *
+     * @param        une référence vers un paramètre de type T.
+     *
+     * @return       un entier représentant le nombre de fois que
+     * 							 l'élément apparaît dans le tableau courant.
+     */
+    int occurrence(const T &element) const;
 
-		/**
-		 * description:  vide le tableau sans libérer la mémoire.
-		 */ 
-		void vider();
+    /**
+     * description:  vide le tableau sans libérer la mémoire.
+     */
+    void vider();
 
-		/**
-		* description:   élimine tous les doublants dans le tableau
-		* 							 courant.
-		*/
-		void unique();
+    /**
+     * description:   élimine tous les doublants dans le tableau
+     * 							 courant.
+     */
+    void unique();
 
-		/**
-		 * description:  surcharge des opérateurs
-		 * 									- d'accès []
-		 * 									- d'accès et de mondification []
-		 * 									- d'égalité ==
-		 * 									- d'affectation de tableau =
-		 */
-    const T & operator [] (int index) const;
-    T & operator [] (int index);
-    bool operator == (const Tableau<T> & autre) const;
-    Tableau<T> & operator = (const Tableau<T> & autre);
+    /**
+     * description:  surcharge des opérateurs
+     * 									- d'accès []
+     * 									- d'accès et de mondification []
+     * 									- d'égalité ==
+     * 									- d'affectation de tableau =
+     */
+    const T &operator[](int index) const;
+    T &operator[](int index);
+    bool operator==(const Tableau<T> &autre) const;
+    Tableau<T> &operator=(const Tableau<T> &autre);
 
-  private:
-    T * elements;
+private:
+    T *elements;
     int nbElements;
     int capacite;
 
-	/**
-	 * description:  surcharge de l'opérateur d'affichage <<. Cet 
-	 * 							 opérateur permettra d'afficher, sur une ligne, 
-	 * 							 les éléments d'un tableau séparé par un 
-	 * 							 point-virgule (;).
-	 */
-	template <typename U>	
-	friend std::ostream & operator << (std::ostream &, const Tableau<U> &);
+    void triRapide(int min, int max);
+
+    int partionner(int min, int max);
+
+    void echanger(T *x, T *y);
+
+    /**
+     * description:  surcharge de l'opérateur d'affichage <<. Cet
+     * 							 opérateur permettra d'afficher, sur une ligne,
+     * 							 les éléments d'un tableau séparé par un
+     * 							 point-virgule (;).
+     */
+    template <typename U>
+    friend std::ostream &operator<<(std::ostream &, const Tableau<U> &);
 };
 
 template <class T>
-Tableau<T>::Tableau(int capacite_initiale) {
-	    capacite = capacite_initiale;
+Tableau<T>::Tableau(int capacite_initiale)
+{
+    capacite = capacite_initiale;
     nbElements = 0;
     elements = new T[capacite];
 }
 
 template <class T>
-Tableau<T>::Tableau(const Tableau & autre) {
-	    capacite = autre.capacite;
+Tableau<T>::Tableau(const Tableau &autre)
+{
+    capacite = autre.capacite;
     nbElements = autre.nbElements;
     elements = new T[capacite];
 
@@ -130,18 +138,21 @@ Tableau<T>::Tableau(const Tableau & autre) {
 }
 
 template <class T>
-Tableau<T>::~Tableau() {
+Tableau<T>::~Tableau()
+{
     delete[] elements;
     elements = nullptr;
 }
 
 template <class T>
-void Tableau<T>::vider() {
+void Tableau<T>::vider()
+{
     nbElements = 0;
 }
 
 template <class T>
-void Tableau<T>::redimensionner(int nouvCapacite){
+void Tableau<T>::redimensionner(int nouvCapacite)
+{
     capacite = nouvCapacite;
     T *tmp = new T[capacite];
     for (int i = 0; i < nbElements; i++)
@@ -154,13 +165,15 @@ void Tableau<T>::redimensionner(int nouvCapacite){
 }
 
 template <class T>
-int Tableau<T>::taille() const {
+int Tableau<T>::taille() const
+{
     return nbElements;
 }
 
 template <class T>
-void Tableau<T>::ajouter(const T & item) {
-    if (nbElements>=capacite)
+void Tableau<T>::ajouter(const T &item)
+{
+    if (nbElements >= capacite)
     {
         redimensionner(capacite * 2);
     }
@@ -168,7 +181,8 @@ void Tableau<T>::ajouter(const T & item) {
 }
 
 template <class T>
-void Tableau<T>::inserer(const T & element, int index) {
+void Tableau<T>::inserer(const T &element, int index)
+{
     // redimensionner
     if (nbElements == capacite)
     {
@@ -186,57 +200,124 @@ void Tableau<T>::inserer(const T & element, int index) {
 }
 
 template <class T>
-void Tableau<T>::enlever(int index) {
-    nbElements--;
-    // decaler
-    for (int i = index; i < nbElements; i++)
+void Tableau<T>::enlever(int index)
+{
+    assert( index >= 0 && index <= nbElements);
+    for (int i = index; i < nbElements - 1; i++)
     {
         elements[i] = elements[i + 1];
     }
+    nbElements--;
 }
 
 template <class T>
-int Tableau<T>::occurrence(const T & element) const {
+int Tableau<T>::occurrence(const T &element) const
+{
     int res = 0;
-    for (int i=0; i<nbElements; i++)
+    for (int i = 0; i < nbElements; i++)
         if (element == elements[i])
-          res++;
+            res++;
     return res;
 }
 
 template <class T>
-void Tableau<T>::unique() {
-	
- for (int i=0; i < nbElements; i++){
-        for(int j=0;j<i;j++){
+void Tableau<T>::echanger(T *x, T *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
-            if(elements[i]==elements[j]){
-                nbElements--;
-                for (int k=i; k<nbElements; k++){
-                    elements[k]=elements[k+1];
-                }
-                i--;
-            }
+template <class T>
+int Tableau<T>::partionner(int min, int max)
+{
+    int pivot = elements[max];
+    int index = min - 1;
+    for (int j = min; j < max; j++)
+    {
+        if (elements[j] < pivot) {
+            ++index;
+            echanger(&elements[index], &elements[j]);
         }
+    }
+    echanger(&elements[index + 1], &elements[max]);
+    return (index + 1);
+}
+
+template <class T>
+void Tableau<T>::triRapide(int min, int max)
+{
+    if(min < max)
+    {
+        int part = partionner(min, max);
+        triRapide(min, part - 1);
+        triRapide(part + 1, max);
     }
 }
 
 template <class T>
-const T & Tableau<T>::operator [] (int index) const {
+void Tableau<T>::unique()
+{
+    // tri insertion -->O(nlogn) et on traverse le tableau par la suite delete doublons.
+    if(taille() <= 1)
+    {
+        return;
+    }
+    triRapide(0, taille() - 1);
+    T temp = elements[0];
+    T *buff = new T[capacite];
+    int j = 0;
+    buff[j] = elements[j];
+    nbElements = 0;
+    for (int i = 1; i < capacite; i++)
+    {
+        if(buff[j] != elements[i])
+        {
+            buff[j + 1] = elements[i];
+            ++nbElements;
+            ++j;
+        }
+    }
+    delete[] elements;
+    elements = buff;
+
+    /*for (int i = 0; i < nbElements; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+
+            if (elements[i] == elements[j])
+            {
+                nbElements--;
+                for (int k = i; k < nbElements; k++)
+                {
+                    elements[k] = elements[k + 1];
+                }
+                i--;
+            }
+        }
+    }*/
+}
+
+template <class T>
+const T &Tableau<T>::operator[](int index) const
+{
     assert(index < nbElements);
     assert(index >= 0);
     return elements[index];
 }
 
 template <class T>
-T & Tableau<T>::operator [] (int index) {
+T &Tableau<T>::operator[](int index)
+{
     assert(index < nbElements);
     assert(index >= 0);
     return elements[index];
 }
 
 template <class T>
-Tableau<T> & Tableau<T>::operator = (const Tableau<T> & autre) {
+Tableau<T> &Tableau<T>::operator=(const Tableau<T> &autre)
+{
     if (this == &autre)
     {
         return *this;
@@ -258,7 +339,8 @@ Tableau<T> & Tableau<T>::operator = (const Tableau<T> & autre) {
 }
 
 template <class T>
-bool Tableau<T>::operator == (const Tableau<T> & autre) const {
+bool Tableau<T>::operator==(const Tableau<T> &autre) const
+{
     // if t1==t2
     if (this == &autre)
     {
@@ -280,13 +362,14 @@ bool Tableau<T>::operator == (const Tableau<T> & autre) const {
 }
 
 template <typename U>
-std::ostream & operator << (std::ostream & os, const Tableau<U> & tab) {
-	
-	for (int i = 0; i < tab.nbElements; i++)
-	{
-		os << tab.elements[i] << ";";
-	}
-	    return os;
+std::ostream &operator<<(std::ostream &os, const Tableau<U> &tab)
+{
+
+    for (int i = 0; i < tab.nbElements; i++)
+    {
+        os << tab.elements[i] << ";";
+    }
+    return os;
 }
 
 #endif
